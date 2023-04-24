9f65bf3ae3bf85ebfeb048b384d6a51955433fe9
TTmikaD <0941116283minhkhanh@gmail.com>
2023-04-24 17:14:46 +0700

    declare and use variables

--- note.js
+++ note.js
@@ -1,4 +1,39 @@
-let yourName = prompt("Hãy nhập tên của bạn");
-// document.write("Xin chào " + yourName);
+// let yourName = prompt("Hãy nhập tên của bạn");
+// // document.write("Xin chào " + yourName);
 
-alert("xin chào" + " " + yourName);
+// alert("xin chào" + " " + yourName);
+
+// Declare and use variables
+// bài 1
+
+// let i = 10;
+// let f = 20.5;
+// let b = true;
+// let s = "Hà Nội";
+
+// document.write("i=" + i);
+// document.write("<br/>")
+// document.write("f =" + f);
+// document.write("<br/>")
+// document.write("b =" + b);
+// document.write("<br/>");
+// document.write("s=" + s);
+
+// // bài 2:
+
+// let width = 20
+// let height = 10
+// let area = width * height
+// document.write('Area = ' + area)
+
+// bài 3
+
+let a = +prompt("nhập số a");
+let b = +prompt("nhập số b");
+
+if (a % b == 0) {
+    alert("a chia hết cho b");
+}
+else {
+    alert ("a không chia hết cho b");
+}
