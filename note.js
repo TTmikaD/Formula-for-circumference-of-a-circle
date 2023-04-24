4c02e57244f023cc69a9fa7f6036e098cb85885e
TTmikaD <0941116283minhkhanh@gmail.com>
2023-04-24 16:55:10 +0700

    đẩy file input and export data

--- note.js
+++ note.js
@@ -1,3 +1,4 @@
-function shownotification() {
-    alert("Hello user !guys");
-}
+let yourName = prompt("Hãy nhập tên của bạn");
+// document.write("Xin chào " + yourName);
+
+alert("xin chào" + " " + yourName);
